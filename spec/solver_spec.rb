e7f9680aae69f09ab79763025a79a74b8d98215b
nimplay <nimrod7day@gmail.com>
2022-10-06 13:56:59 -0400

    fixing linters errors

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,18 +5,18 @@ describe Solver do
 
   context 'Factorial method tests' do
     it 'Should return 1 if the input is 0' do
-      factorial_0 = solver.factorial(0)
-      expect(factorial_0).to eq(1)
+      factorial0 = solver.factorial(0)
+      expect(factorial0).to eq(1)
     end
     it 'Should raise an error if the number is less than 0' do
       expect { solver.factorial(-1) }.to raise_error(ArgumentError)
     end
     it 'Should return the multiplication of all integers from 1 to the input' do
-      factorial_3 = solver.factorial(3)
-      factorial_4 = solver.factorial(4)
+      factorial3 = solver.factorial(3)
+      factorial4 = solver.factorial(4)
 
-      expect(factorial_3).to eq(6)
-      expect(factorial_4).to eq(24)
+      expect(factorial3).to eq(6)
+      expect(factorial4).to eq(24)
     end
   end
 
@@ -39,25 +39,25 @@ describe Solver do
     end
 
     it "Should return 'fizz' if the integer is divisible by 3" do
-      fizzbuzz_6 = solver.fizzbuzz(6)
-      expect(fizzbuzz_6).to eq('fizz')
+      fizzbuzz6 = solver.fizzbuzz(6)
+      expect(fizzbuzz6).to eq('fizz')
     end
 
     it "Should return 'buzz' if the integer is divisible by 5" do
-      fizzbuzz_10 = solver.fizzbuzz(10)
-      expect(fizzbuzz_10).to eq('buzz')
+      fizzbuzz10 = solver.fizzbuzz(10)
+      expect(fizzbuzz10).to eq('buzz')
     end
 
     it "Should return 'fizzbuzz' if the integer is divisible by 3 and by 5" do
-      fizzbuzz_15 = solver.fizzbuzz(15)
-      expect(fizzbuzz_15).to eq('fizzbuzz')
+      fizzbuzz15 = solver.fizzbuzz(15)
+      expect(fizzbuzz15).to eq('fizzbuzz')
     end
 
     it 'Should return a String' do
       fizzbuzz_type = solver.fizzbuzz(7).is_a?(String)
-      fizzbuzz_7 = solver.fizzbuzz(7)
+      fizzbuzz7 = solver.fizzbuzz(7)
       expect(fizzbuzz_type).to eq(false)
-      expect(fizzbuzz_7).to eq(7)
+      expect(fizzbuzz7).to eq(7)
     end
   end
 end
